5f020fefbf5ef11e8b7e3c1fbb84c8d26cf4b46a
Ohemaafos <129678617+Ohemaafos@users.noreply.github.com>
2023-04-02 15:50:28 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+#string
+##a = 2
+#b = 5
+#c= str(a+b)
+#f = c+"5"
+#print("The answer is + "+ f +"  adwoa")
+
+#print(f)
+
+#input
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+c=int(input("Enter the value of c: "))
+d=a*b*c
+print("The answer is "+str(d))
+print (f"The answer for {x}+{y} is {z}")
+
+
